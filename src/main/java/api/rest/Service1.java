adfe9b12ad9340d712578f6720c8b85d66192d68
Gökhan Ercan <gokhan@ercannet.com>
2021-10-04 01:25:14 +0300

    Added method with parameters

--- src/main/java/api/rest/Service1.java
+++ src/main/java/api/rest/Service1.java
@@ -3,16 +3,18 @@ package api.rest;
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 
 @Path("/service1")
 @Singleton
 public class Service1 extends RestBase {
 
-    //see on http://localhost:2223/rest/service1/method1
+    //request http://localhost:2223/rest/service1/method1?p1=gokhan&p2=ercan
     @GET
     @Path("method1")
-    public String Method1() {
-        return "service1.method1 result";
+    public String Method1(@QueryParam("p1")String p1, @QueryParam("p2")String p2)
+    {
+        return p1 + " " + p2;
     }
 
     @GET
